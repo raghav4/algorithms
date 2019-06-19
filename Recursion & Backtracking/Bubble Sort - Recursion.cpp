12b65a4260c7e6a2458c126f31b2eb4372d2cf4c
Raghav Sharma <raghavsyt@gmail.com>
2019-06-19 10:35:03 +0530

    Update Bubble Sort - Recursion.cpp

--- Recursion & Backtracking/Bubble Sort - Recursion.cpp
+++ Recursion & Backtracking/Bubble Sort - Recursion.cpp
@@ -1,23 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void bubbleSort(int *arr, int n)
+void bubbleSort(int *arr, int i, int n)
 {
-	if(n<=1){
-		return;
-	}
-	for(int i=0;i<=n-2;i++){
-			if(arr[i]>arr[i+1]){
-			swap(arr[i],arr[i+1]);
-		}
-	}
-	bubbleSort(arr,n-1);
+    if(n<=1){
+        return;
+    }
+    if(i==n-1){
+        // if i is at last element then our largest element is at last, now do so for other elements as well.
+        bubbleSort(arr,0,n-1);
+        return;
+    }
+    if(arr[i]>arr[i+1]){
+        swap(arr[i],arr[i+1]);
+    }
+    bubbleSort(arr,i+1,n);
 }
-int main()
-{
-	int arr[]={4,3,2,0,1};
-	int n = sizeof(arr)/sizeof(int);
-	bubbleSort(arr,n);
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
-	}
+int main() {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    bubbleSort(a,0,n);
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
 }
