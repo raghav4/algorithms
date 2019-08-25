0a08a96df01b36fdabb4cb27a91d59a1b39086f2
Raghav <raghavsyt@gmail.com>
2019-08-25 17:24:01 +0530

    updated sieve @25-08-19

--- Number Theory/Sieve of Eratosthenes.cpp
+++ Number Theory/Sieve of Eratosthenes.cpp
@@ -1,37 +1,41 @@
-// Working till 10^4, afterwards Segmantation Fault: 11
-#include<iostream>
-#define N 10000000
+#include<bits/stdc++.h>
+#define ll long long
+#define MAX 100001 
 using namespace std;
-int p[N+1] = {0};
-void GenerateSieve(int n)
-{
-	p[0] = p[1] = 0; // 0 and 1 are not prime
-	p[2] = 1; // 2 is prime
+vector<int> sieve(){
+	vector<int>isPrime(MAX+1);
+	
+	// Marking all the numbers as prime initially.
+	fill(isPrime.begin(), isPrime.end(), 1);
 
-	// Marking all the odd numbers as prime initially
-	for(int i=3;i<=n;i+=2){
-		p[i] = 1;
-	}
-
-	// Marking non prime odd numbers as 0
-	for(int i=3;i<=n;i+=2){
-		if(p[i]){
-		// Marking the multiples of that numbers as not prime.
-			for(int j=i*i;j<=n;j+=(2*i)){ // Optimised 2i -> i*i, j+=i -> j+=2i
-				p[j] = 0;
+	// Sieve Filling
+	for(int i=2;i*i<=MAX;i++){
+		if(isPrime[i]){
+			for(int j=i*i;j<=MAX;j+=i){
+				isPrime[j] = 0;
 			}
 		}
 	}
+	// 
+	return isPrime;
+
 }
 int main()
 {
 	int n;
 	cin>>n;
-	GenerateSieve(n);
-	for(int i=0;i<=N;i++){
-		if(p[i]){
+	vector<int>primes = sieve();
+	for(int i=1;i<=n;i++){
+		if(primes[i]){
 			cout<<i<<" ";
 		}
 	}
-	cout<<endl;
 }
+
+/*
+If you observe that the first multiple of each prime number that is to be struck out is that square of the number.
+We''ll use this in our code :). You can also mathematically prove this.
+Conversely we'll also only strike out multiples of numbers that are under sqrt(n) 
+as the ones larger than it will be the prime numbers left which have no multiples under n.
+*/
+
